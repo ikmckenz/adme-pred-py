b84b01e5934f35d9cf2b60f7736aebbd05fcfea7
Ian McKenzie <sirreal.ian@gmail.com>
2019-11-30 15:35:41 -0500

    Integrate boiled egg into ADME class

--- src/adme_pred.py
+++ src/adme_pred.py
@@ -1,8 +1,7 @@
-from rdkit import Chem
-from rdkit.Chem import Descriptors
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
-import numpy as np
+from rdkit import Chem
+from rdkit.Chem import Descriptors
 
 
 class ADME(object):
@@ -13,6 +12,35 @@ class ADME(object):
         else:
             self.mol = mol
 
+    def boiled_egg(self):
+        fig, ax = plt.subplots()
+
+        ax.patch.set_facecolor("lightgrey")
+
+        white = Ellipse((71.051, 2.292), 142.081, 8.740, -1.031325)
+        yolk = Ellipse((38.117, 3.177), 82.061, 5.557, -0.171887)
+
+        white.set_clip_box(ax.bbox)
+        white.set_facecolor("white")
+        ax.add_artist(white)
+
+        yolk.set_clip_box(ax.bbox)
+        yolk.set_facecolor("orange")
+        ax.add_artist(yolk)
+
+        ax.set_xlim(-10, 200)
+        ax.set_ylim(-4, 8)
+
+        ax.set_xlabel("TPSA")
+        ax.set_ylabel("LOGP")
+
+        logp = self.logp()
+        tpsa = self.tpsa()
+
+        ax.scatter(tpsa, logp, zorder=10)
+
+        return fig
+
     def logp(self):
         return Descriptors.MolLogP(self.mol)
 
@@ -20,33 +48,9 @@ class ADME(object):
         return Descriptors.TPSA(self.mol)
 
 
-chem = "ClC1=CC2=C(C=C1)N3C(C)=NN=C3CN=C2C4=CC=CC=C4"
-mol = ADME(chem)
+if __name__ == "__main__":
+    chem = "ClC1=CC2=C(C=C1)N3C(C)=NN=C3CN=C2C4=CC=CC=C4"
+    mol = ADME(chem)
 
-fig, ax = plt.subplots()
-
-ax.patch.set_facecolor("lightgrey")
-
-white = Ellipse((71.051, 2.292), 142.081, 8.740, -1.031325)
-yolk = Ellipse((38.117, 3.177), 82.061, 5.557, -0.171887)
-
-white.set_clip_box(ax.bbox)
-white.set_facecolor("white")
-ax.add_artist(white)
-
-yolk.set_clip_box(ax.bbox)
-yolk.set_facecolor("orange")
-ax.add_artist(yolk)
-
-ax.set_xlim(-10, 200)
-ax.set_ylim(-4, 8)
-
-ax.set_xlabel("TPSA")
-ax.set_ylabel("LOGP")
-
-logp = mol.logp()
-tpsa = mol.tpsa()
-
-ax.scatter(tpsa, logp, zorder=10)
-
-plt.show()
+    fig = mol.boiled_egg()
+    plt.show()
