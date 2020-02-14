566466dce2e178e21cf646d6e54952106dddc636
Ian McKenzie <sirreal.ian@gmail.com>
2020-02-13 22:15:12 -0500

    Implement Ghose druglikeness filter

--- src/adme_pred.py
+++ src/adme_pred.py
@@ -12,7 +12,7 @@ class ADME(object):
         else:
             self.mol = mol
 
-    def lipinski_druglikeness(self, verbose=False):
+    def druglikeness_lipinski(self, verbose=False):
         violations = []
 
         h_bond_donors = self.h_bond_donors()
@@ -23,13 +23,37 @@ class ADME(object):
         if h_bond_acceptors > 10:
             violations.append("H Bond Acceptors {}>10".format(h_bond_acceptors))
 
-        molecular_mass = self.molecular_mass()
-        if molecular_mass > 500:
-            violations.append("Molecular Mass {}>500".format(molecular_mass))
+        molecular_weight = self.molecular_weight()
+        if molecular_weight > 500:
+            violations.append("Molecular Weight {}>500".format(molecular_weight))
 
         logp = self.logp()
         if logp > 5:
             violations.append("LOGP {}>5".format(logp))
+
+        if verbose:
+            return violations
+        else:
+            return len(violations) < 1
+
+    def druglikeness_ghose(self, verbose=False):
+        violations = []
+
+        logp = self.logp()
+        if logp > 5.6 or logp < -0.4:
+            violations.append("LOGP {}".format(logp))
+
+        molecular_mass = self.molecular_weight()
+        if molecular_mass < 160 or molecular_mass > 480:
+            violations.append("Molecular Mass {}".format(molecular_mass))
+
+        molar_refractivity = self.molar_refractivity()
+        if molar_refractivity < 40 or molar_refractivity > 130:
+            violations.append("MR {}".format(molar_refractivity))
+
+        n_atoms = self.n_atoms()
+        if n_atoms < 20 or n_atoms > 70:
+            violations.append("N Atoms {}".format(n_atoms))
 
         if verbose:
             return violations
@@ -71,8 +95,14 @@ class ADME(object):
     def h_bond_acceptors(self):
         return Chem.Lipinski.NumHAcceptors(self.mol)
 
-    def molecular_mass(self):
+    def molar_refractivity(self):
+        return Chem.Crippen.MolMR(self.mol)
+
+    def molecular_weight(self):
         return Descriptors.ExactMolWt(self.mol)
+
+    def n_atoms(self):
+        return self.mol.GetNumAtoms()
 
     def logp(self):
         return Descriptors.MolLogP(self.mol)
@@ -85,4 +115,4 @@ if __name__ == "__main__":
     chem = "ClC1=CC2=C(C=C1)N3C(C)=NN=C3CN=C2C4=CC=CC=C4"
     mol = ADME(chem)
 
-    print(mol.lipinski_druglikeness())
+    print(mol.druglikeness_ghose())
