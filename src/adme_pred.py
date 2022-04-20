6f4bafb245f4e9259a2d03015279df94b026bf2c
Ian McKenzie <sirreal.ian@gmail.com>
2022-04-20 14:47:38 -0700

    initial work on pylint changes

--- src/adme_pred.py
+++ src/adme_pred.py
@@ -8,7 +8,7 @@ from rdkit.Chem import FilterCatalog
 from rdkit.Chem import rdqueries
 
 
-class ADME(object):
+class ADME:
     """
     ADME is the main class that contains the core ADME calculations. Functions
     include many druglikeness filters (Lipinski's rule of five, Egan, Ghose,
@@ -17,21 +17,23 @@ class ADME(object):
     """
 
     # Class constants
-    BOILED_EGG_HIA_ELLIPSE = Ellipse((71.051, 2.292), 142.081, 8.740, -1.031325)  # From the BOILED-Egg paper
-    BOILED_EGG_BBB_ELLIPSE = Ellipse((38.117, 3.177), 82.061, 5.557, -0.171887)  # From the BOILED-Egg paper
 
-    def __init__(self, mol):
-        if isinstance(mol, str):
-            self.mol = Chem.MolFromSmiles(mol)
+    # From the BOILED-Egg paper
+    BOILED_EGG_HIA_ELLIPSE = Ellipse((71.051, 2.292), 142.081, 8.740, -1.031325)
+    BOILED_EGG_BBB_ELLIPSE = Ellipse((38.117, 3.177), 82.061, 5.557, -0.171887)
+
+    def __init__(self, _mol):
+        if isinstance(_mol, str):
+            self.mol = Chem.MolFromSmiles(_mol)
         else:
-            self.mol = mol
+            self.mol = _mol
 
     def full_report(self):
         """Print a full report on a molecule with many implemented functions"""
 
         print("Pharmacokinetics:")
-        print("GI absorption: {}".format("High" if self.boiled_egg_bbb() else "Low"))
-        print("BBB permeant: {}".format("Yes" if self.boiled_egg_hia() else "No"))
+        print("GI absorption: High" if self.boiled_egg_bbb() else "GI absorption: Low")
+        print("BBB permeant: Yes" if self.boiled_egg_hia() else "BBB permeant: No")
         print(os.linesep)
 
         druglikeness = self.druglikeness_lipinski(verbose=True)
@@ -43,8 +45,8 @@ class ADME(object):
         print(os.linesep)
 
         print("Medicinal Chemistry:")
-        print("PAINS filter: {}".format(self.pains()))
-        print("Brenk filter: {}".format(self.brenk()))
+        print(f"PAINS filter: {self.pains()}")
+        print(f"Brenk filter: {self.brenk()}")
 
     def druglikeness_egan(self, verbose=False):
         """
@@ -61,18 +63,18 @@ class ADME(object):
         """
         violations = []
 
-        tpsa = self.tpsa()
+        tpsa = self._tpsa()
         if tpsa > 131.6:
-            violations.append("PSA {}".format(tpsa))
+            violations.append(f"PSA {tpsa}")
 
-        logp = self.logp()
+        logp = self._logp()
         if logp > 5.88:
-            violations.append("logP {}".format(logp))
+            violations.append(f"logP {logp}")
 
         if verbose:
             return violations
-        else:
-            return len(violations) < 1
+
+        return len(violations) < 1
 
     def druglikeness_ghose(self, verbose=False):
         """
@@ -90,26 +92,26 @@ class ADME(object):
         """
         violations = []
 
-        logp = self.logp()
+        logp = self._logp()
         if logp > 5.6 or logp < -0.4:
-            violations.append("LOGP {}".format(logp))
+            violations.append(f"LOGP {logp}")
 
-        molecular_weight = self.molecular_weight()
+        molecular_weight = self._molecular_weight()
         if molecular_weight < 160 or molecular_weight > 480:
-            violations.append("Molecular Mass {}".format(molecular_weight))
+            violations.append(f"Molecular Mass {molecular_weight}")
 
-        molar_refractivity = self.molar_refractivity()
+        molar_refractivity = self._molar_refractivity()
         if molar_refractivity < 40 or molar_refractivity > 130:
-            violations.append("MR {}".format(molar_refractivity))
+            violations.append(f"MR {molar_refractivity}")
 
-        n_atoms = self.n_atoms()
+        n_atoms = self._n_atoms()
         if n_atoms < 20 or n_atoms > 70:
-            violations.append("N Atoms {}".format(n_atoms))
+            violations.append(f"N Atoms {n_atoms}")
 
         if verbose:
             return violations
-        else:
-            return len(violations) < 1
+
+        return len(violations) < 1
 
     def druglikeness_ghose_pref(self, verbose=False):
         """
@@ -127,26 +129,26 @@ class ADME(object):
         """
         violations = []
 
-        logp = self.logp()
+        logp = self._logp()
         if logp > 4.1 or logp < 1.3:
-            violations.append("LOGP {}".format(logp))
+            violations.append(f"LOGP {logp}")
 
-        molecular_weight = self.molecular_weight()
+        molecular_weight = self._molecular_weight()
         if molecular_weight < 230 or molecular_weight > 390:
-            violations.append("Molecular Mass {}".format(molecular_weight))
+            violations.append(f"Molecular Mass {molecular_weight}")
 
-        molar_refractivity = self.molar_refractivity()
+        molar_refractivity = self._molar_refractivity()
         if molar_refractivity < 70 or molar_refractivity > 110:
-            violations.append("MR {}".format(molar_refractivity))
+            violations.append(f"MR {molar_refractivity}")
 
-        n_atoms = self.n_atoms()
+        n_atoms = self._n_atoms()
         if n_atoms < 30 or n_atoms > 55:
-            violations.append("N Atoms {}".format(n_atoms))
+            violations.append(f"N Atoms {n_atoms}")
 
         if verbose:
             return violations
-        else:
-            return len(violations) < 1
+
+        return len(violations) < 1
 
     def druglikeness_lipinski(self, verbose=False):
         """
@@ -163,28 +165,28 @@ class ADME(object):
         """
         violations = []
 
-        h_bond_donors = self.h_bond_donors()
+        h_bond_donors = self._h_bond_donors()
         if h_bond_donors > 5:
-            violations.append("H Bond Donors {}>5".format(h_bond_donors))
+            violations.append(f"H Bond Donors {h_bond_donors}>5")
 
-        h_bond_acceptors = self.h_bond_acceptors()
+        h_bond_acceptors = self._h_bond_acceptors()
         if h_bond_acceptors > 10:
-            violations.append("H Bond Acceptors {}>10".format(h_bond_acceptors))
+            violations.append(f"H Bond Acceptors {h_bond_acceptors}>10")
 
-        molecular_weight = self.molecular_weight()
+        molecular_weight = self._molecular_weight()
         if molecular_weight > 500:
-            violations.append("Molecular Weight {}>500".format(molecular_weight))
+            violations.append(f"Molecular Weight {molecular_weight}>500")
 
-        logp = self.logp()
+        logp = self._logp()
         if logp > 5:
-            violations.append("LOGP {}>5".format(logp))
+            violations.append(f"LOGP {logp}>5")
 
         if verbose:
             if not violations:
                 return "No violations found"
             return violations
-        else:
-            return len(violations) < 1
+
+        return len(violations) < 1
 
     def druglikeness_muegge(self, verbose=False):
         """
@@ -197,46 +199,46 @@ class ADME(object):
         """
         violations = []
 
-        molecular_weight = self.molecular_weight()
+        molecular_weight = self._molecular_weight()
         if molecular_weight > 600 or molecular_weight < 200:
-            violations.append("MW {}".format(molecular_weight))
+            violations.append(f"MW {molecular_weight}")
 
-        logp = self.logp()
+        logp = self._logp()
         if logp > 5 or logp < -2:
-            violations.append("LOGP {}".format(logp))
+            violations.append(f"LOGP {logp}")
 
-        tpsa = self.tpsa()
+        tpsa = self._tpsa()
         if tpsa > 150:
-            violations.append("TPSA {}".format(tpsa))
+            violations.append(f"TPSA {tpsa}")
 
-        n_rings = self.n_rings()
+        n_rings = self._n_rings()
         if n_rings > 7:
-            violations.append("N Rings {}".format(n_rings))
+            violations.append(f"N Rings {n_rings}")
 
-        n_carbon = self.n_carbons()
+        n_carbon = self._n_carbons()
         if n_carbon < 5:
-            violations.append("N Carbon {}".format(n_carbon))
+            violations.append(f"N Carbon {n_carbon}")
 
-        n_heteroatoms = self.n_heteroatoms()
+        n_heteroatoms = self._n_heteroatoms()
         if n_heteroatoms < 2:
-            violations.append("N Heteroatoms".format(n_heteroatoms))
+            violations.append(f"N Heteroatoms {n_heteroatoms}")
 
-        n_rot_bonds = self.n_rot_bonds()
+        n_rot_bonds = self._n_rot_bonds()
         if n_rot_bonds > 15:
-            violations.append("N Rot Bonds {}".format(n_rot_bonds))
+            violations.append(f"N Rot Bonds {n_rot_bonds}")
 
-        h_bond_acc = self.h_bond_acceptors()
+        h_bond_acc = self._h_bond_acceptors()
         if h_bond_acc > 10:
-            violations.append("H Bond Acc {}".format(h_bond_acc))
+            violations.append(f"H Bond Acc {h_bond_acc}")
 
-        h_bond_don = self.h_bond_donors()
+        h_bond_don = self._h_bond_donors()
         if h_bond_don > 5:
-            violations.append("H Bond Don {}".format(h_bond_don))
+            violations.append(f"H Bond Don {h_bond_don}")
 
         if verbose:
             return violations
-        else:
-            return len(violations) < 1
+
+        return len(violations) < 1
 
     def druglikeness_veber(self, verbose=False):
         """
@@ -255,18 +257,18 @@ class ADME(object):
         """
         violations = []
 
-        tpsa = self.tpsa()
+        tpsa = self._tpsa()
         if tpsa > 140:
-            violations.append("TPSA {}".format(tpsa))
+            violations.append(f"TPSA {tpsa}")
 
-        n_rot_bonds = self.n_rot_bonds()
+        n_rot_bonds = self._n_rot_bonds()
         if n_rot_bonds > 10:
-            violations.append("N Rotatable Bonds {}".format(n_rot_bonds))
+            violations.append(f"N Rotatable Bonds {n_rot_bonds}")
 
         if verbose:
             return violations
-        else:
-            return len(violations) < 1
+
+        return len(violations) < 1
 
     def boiled_egg_bbb(self, logp=None, psa=None):
         """
@@ -279,10 +281,10 @@ class ADME(object):
         """
 
         if logp is None:
-            logp = self.logp()
+            logp = self._logp()
 
         if psa is None:
-            psa = self.tpsa()
+            psa = self._tpsa()
 
         return self.BOILED_EGG_BBB_ELLIPSE.contains_point((psa, logp))
 
@@ -293,14 +295,14 @@ class ADME(object):
 
         This multivariate model uses log P and Polar Surface Area to determine
         druglikeness. This function implements their Human Intestinal Absorption
-        (HIA) model, which is the "white" of the BOILED-Egg. 
+        (HIA) model, which is the "white" of the BOILED-Egg.
         """
 
         if logp is None:
-            logp = self.logp()
+            logp = self._logp()
 
         if psa is None:
-            psa = self.tpsa()
+            psa = self._tpsa()
 
         return self.BOILED_EGG_HIA_ELLIPSE.contains_point((psa, logp))
 
@@ -313,31 +315,31 @@ class ADME(object):
         boiled_egg_hia and creates a matplotlib plot of the computations.
         :return:
         """
-        fig, ax = plt.subplots()
+        fig, axis = plt.subplots()
 
-        ax.patch.set_facecolor("lightgrey")
+        axis.patch.set_facecolor("lightgrey")
 
         white = self.BOILED_EGG_HIA_ELLIPSE
         yolk = self.BOILED_EGG_BBB_ELLIPSE
 
-        white.set_clip_box(ax.bbox)
+        white.set_clip_box(axis.bbox)
         white.set_facecolor("white")
-        ax.add_artist(white)
+        axis.add_artist(white)
 
-        yolk.set_clip_box(ax.bbox)
+        yolk.set_clip_box(axis.bbox)
         yolk.set_facecolor("orange")
-        ax.add_artist(yolk)
+        axis.add_artist(yolk)
 
-        ax.set_xlim(-10, 200)
-        ax.set_ylim(-4, 8)
+        axis.set_xlim(-10, 200)
+        axis.set_ylim(-4, 8)
 
-        ax.set_xlabel("TPSA")
-        ax.set_ylabel("LOGP")
+        axis.set_xlabel("TPSA")
+        axis.set_ylabel("LOGP")
 
-        logp = self.logp()
-        tpsa = self.tpsa()
+        logp = self._logp()
+        tpsa = self._tpsa()
 
-        ax.scatter(tpsa, logp, zorder=10)
+        axis.scatter(tpsa, logp, zorder=10)
 
         return fig
 
@@ -375,38 +377,49 @@ class ADME(object):
         catalog = FilterCatalog.FilterCatalog(params)
         return catalog.HasMatch(self.mol)
 
-    def h_bond_donors(self):
+    def _h_bond_donors(self) -> int:
+        """Number of Hydrogen Bond Donors"""
         return Chem.Lipinski.NumHDonors(self.mol)
 
-    def h_bond_acceptors(self):
+    def _h_bond_acceptors(self) -> int:
+        """Number of Hydrogen Bond Acceptors"""
         return Chem.Lipinski.NumHAcceptors(self.mol)
 
-    def molar_refractivity(self):
+    def _molar_refractivity(self):
+        """Wildman-Crippen Molar Refractivity"""
         return Chem.Crippen.MolMR(self.mol)
 
-    def molecular_weight(self):
+    def _molecular_weight(self):
+        """Molecular weight"""
         return Descriptors.ExactMolWt(self.mol)
 
-    def n_atoms(self):
+    def _n_atoms(self):
+        """Number of atoms"""
         return self.mol.GetNumAtoms()
 
-    def n_carbons(self):
+    def _n_carbons(self):
+        """Number of carbon atoms"""
         carbon = Chem.rdqueries.AtomNumEqualsQueryAtom(6)
         return len(self.mol.GetAtomsMatchingQuery(carbon))
 
-    def n_heteroatoms(self):
+    def _n_heteroatoms(self):
+        """Number of heteroatoms"""
         return Descriptors.rdMolDescriptors.CalcNumHeteroatoms(self.mol)
 
-    def n_rings(self):
+    def _n_rings(self):
+        """Number of rings"""
         return Descriptors.rdMolDescriptors.CalcNumRings(self.mol)
 
-    def n_rot_bonds(self):
+    def _n_rot_bonds(self):
+        """Number of rotatable bonds"""
         return Chem.Lipinski.NumRotatableBonds(self.mol)
 
-    def logp(self):
+    def _logp(self):
+        """Log of partition coefficient"""
         return Descriptors.MolLogP(self.mol)
 
-    def tpsa(self):
+    def _tpsa(self):
+        """Topological polar surface area"""
         return Descriptors.TPSA(self.mol)
 
 
@@ -414,3 +427,5 @@ if __name__ == "__main__":
     chem = "O=C(C)Oc1ccccc1C(=O)O"
     mol = ADME(chem)
     mol.full_report()
+    for s in mol.druglikeness_muegge(verbose=True):
+        print(s)
